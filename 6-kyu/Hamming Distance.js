dc7f5c37a16300753cf6f5d5f6257c853c62774c
EmpiresDesMachines <empiresdesmachines@gmail.com>
2021-11-22 04:28:01 +0300

    Hamming Distance

---
+++ 6-kyu/Hamming Distance.js
@@ -0,0 +1,13 @@
+/*
+The Hamming Distance is a measure of similarity between two strings of equal length. Complete the function so that it returns the number of positions where the input strings do not match.
+
+Examples (in JavaScript):
+
+hamming("I like turtles","I like turkeys")  //returns 3
+hamming("Hello World","Hello World")  //returns 0
+
+You can assume that the two input strings are of equal length.
+
+*/
+
+const hamming = (a, b) => a.split('').filter((a, i) => a !== b[i]).length;
