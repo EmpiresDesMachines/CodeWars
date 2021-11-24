8252e75e2049f60fc65e41dbb773250e4bcbd17f
EmpiresDesMachines <empiresdesmachines@gmail.com>
2021-11-24 03:23:56 +0300

    Once

---
+++ 6-kyu/Once.js
@@ -0,0 +1,22 @@
+/*
+You'll implement once, a function that takes another function as an argument, and returns a new version of that function that can only be called once.
+
+Subsequent calls to the resulting function should have no effect (and should return undefined).
+
+For example:
+
+logOnce = once(console.log)
+logOnce("foo") // -> "foo"
+logOnce("bar") // -> no effect
+
+*/
+
+function once(fn) {
+  let once = true;
+  return function (...args) {
+    if (once) {
+      once = false;
+      return fn(...args);
+    }
+  };
+}
