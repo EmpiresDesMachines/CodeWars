175dd092577ec71489e7e70ecb1d00254e40d93e
EmpiresDesMachines <empiresdesmachines@gmail.com>
2021-12-03 03:28:53 +0300

    Object extend

---
+++ 5-kyu/Object extend.js
@@ -0,0 +1,30 @@
+/*
+Let's make a function that returns a new object, containing all of the properties of any objects passed in as parameters.
+
+The returned object should include the first instance of each property seen on any parameter object, and any other instance of that property should be ignored.
+
+Also, if any parameter is not an object, it should be ignored. You can use the function isObject(object) to determine if a parameter is an object or not (it will return true or false).
+
+extend( {a: 1, b: 2}, {c: 3} ) // should === {a: 1, b: 2, c: 3}
+extend( {a: 1, b: 2}, {c: 3}, {d: 4} ) // should === {a: 1, b: 2, c: 3, d: 4}
+extend( {a: 1, b: 2}, {a: 3, c: 3} ) // should  === {a: 1, b: 2, c: 3}
+extend( {a: false, b: null}, {a: true, b: 2, c: 3} ) // should  === {a: false, b: null, c: 3}
+extend( {a: 1, b: 2}, {c: 3} , {d: 5} ) // should === {a: 1, b: 2, c: 3, d: 4}
+
+*/
+
+const extend = function (...params) {
+  const obj = {};
+
+  for (let i = 0; i < params.length; i++) {
+    if (typeof params[i] === 'object') {
+      const object = params[i];
+
+      for (const [key, value] of Object.entries(object)) {
+        if (obj[key] === undefined) obj[key] = value;
+      }
+    }
+  }
+
+  return obj;
+};
