7ffd0efdc948659420c1f8b15dab1f5e16717203
EmpiresDesMachines <empiresdesmachines@gmail.com>
2022-02-13 03:37:31 +0300

    Write JavaScript's 'call' function using apply.

---
+++ 6-kyu/Write JavaScript's 'call' function using apply.js
@@ -0,0 +1,12 @@
+/* 
+Imagine JavaScript didn't natively include the call function. The apply function however still exists.
+
+Using apply, write call.
+
+Note: console.log internally uses the 'call' function, which therefore means you can't debug using console.log as it will either call an empty function or cause an infinite loop.
+
+*/
+
+Function.prototype.call = function (thisArg, ...args) {
+  return this.apply(thisArg, args);
+};
