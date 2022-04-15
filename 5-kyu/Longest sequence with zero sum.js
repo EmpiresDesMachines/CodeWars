c6566f39c70695c90c26200ee591c6247f8d3db1
EmpiresDesMachines <empiresdesmachines@gmail.com>
2022-04-15 11:31:05 +0300

    Longest sequence with zero sum

--- 5-kyu/Longest sequence with zero sum.js
+++ 5-kyu/Longest sequence with zero sum.js
@@ -1,3 +1,12 @@
+/*
+Write a method which takes an array of integers (positive and negative) and returns the longest contiguous sequence in this array, which total sum of elements equal 0.
+
+For example:
+maxZeroSequenceLength([25, -35, 12, 6, 92, -115, 17, 2, 2, 2, -7, 2, -9, 16, 2, -11])
+
+Should return [92, -115, 17, 2, 2, 2], because this is the longest zero-sum sequence in the array.
+*/
+
 const maxZeroSequence = (arr) => {
   let max = [];
   for (let i = arr.length - 1; i >= 0; i--) {
