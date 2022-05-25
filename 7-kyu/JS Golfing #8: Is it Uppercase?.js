5bf73fb568fd01e5bc99874e422f2960b3daf99f
EmpiresDesMachines <empiresdesmachines@gmail.com>
2022-05-25 11:05:32 +0300

    JS Golfing #8: Is it Uppercase?

---
+++ 7-kyu/JS Golfing #8: Is it Uppercase?.js
@@ -0,0 +1,9 @@
+/*
+Task:
+
+Check if the input ( one letter ) is Uppercase with no more than 9 characters.
+
+No need to check for invalid input. Only letters will be tested. Return true if Uppercase and false if Lowercase.
+*/
+
+f=s=>s<{}
