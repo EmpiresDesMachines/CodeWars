3cd8a6f25abd8244aa40c9ff017f03b785c801cb
EmpiresDesMachines <empiresdesmachines@gmail.com>
2023-07-22 20:30:37 +0300

    One Line Task: Two Letters

---
+++ 5-kyu/One Line Task: Two Letters.js
@@ -0,0 +1,6 @@
+/*
+TASK: Convert two letters to their ASCII code(number format) and your code should in one line and less than 54 characters.
+*/
+
+// prettier-ignore
+toASCIINumber=(a,b)=>a[v='charCodeAt']()+''+b[v]()|0
