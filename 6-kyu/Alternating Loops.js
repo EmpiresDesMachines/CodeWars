8d84fc9cc22bc9f6b4c450f2ed8cb4142ea0116f
EmpiresDesMachines <empiresdesmachines@gmail.com>
2024-03-07 21:52:46 +0300

    Alternating Loops

---
+++ 6-kyu/Alternating Loops.js
@@ -0,0 +1,19 @@
+/*
+Write
+
+function combine()
+that combines arrays by alternatingly taking elements passed to it.
+
+E.g
+
+combine(['a', 'b', 'c'], [1, 2, 3]) == ['a', 1, 'b', 2, 'c', 3]
+combine(['a', 'b', 'c'], [1, 2, 3, 4, 5]) == ['a', 1, 'b', 2, 'c', 3, 4, 5]
+combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8]) == ['a', 1, 6, 8, 'b', 2, 7, 'c', 3, 4, 5]
+Arrays can have different lengths.
+*/
+
+const combine = (...args) =>
+  new Array(Math.max(...args.map((v) => v.length)))
+    .fill()
+    .reduce((acc, v, i) => acc.concat(...args.map((v) => v[i])), [])
+    .filter(Boolean);
